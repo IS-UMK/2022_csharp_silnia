e455a104fb34ec4d9f22f4e80d9290037bc179b4
pk <pk@PK2-00>
2022-11-05 16:24:09 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ProgramSilnia
+{
+    class Program
+    {
+        static int Silnia(int n)
+        {
+            int s = 1;
+
+            for (int i = 2; i <= n; i++)
+            {
+                s = s * i;
+            }
+            return s;
+        }
+
+        static int SilniaRekurencja(int n)
+        {
+            if (n < 2) return 1;
+            return SilniaRekurencja(n - 1) * n;
+        }
+
+        public static void Main()
+        {
+            int n;
+
+            Console.Write("n=");
+            n = int.Parse(Console.ReadLine());
+            
+            for (int i = 1; i <= n; i++)
+            {
+                int s = Silnia(i);
+                Console.WriteLine("Silnia z {0} wynosi {1}", i, s);
+            }
+
+            Console.WriteLine("To samo wykorzystując wersję rekurencyjną");
+            for (int i = 1; i <= n; i++)
+            {
+                int s = SilniaRekurencja(i);
+                Console.WriteLine("Silnia z {0} wynosi {1}", i, s);
+            }
+        }
+    }
+}
